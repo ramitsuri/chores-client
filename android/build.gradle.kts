0734539e1ac11cac0c4bd405ad3eb82c7c93f02e
Ramit Suri <ramitsuri007@gmail.com>
2022-11-27 14:31:57 -0500

    Bump version to 4.9(34)

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -68,8 +68,8 @@ android {
         applicationId = "com.ramitsuri.choresclient.android"
         minSdk = 26
         targetSdk = 33
-        versionCode = 33
-        versionName = "4.8"
+        versionCode = 34
+        versionName = "4.9"
 
         kapt {
             arguments {
