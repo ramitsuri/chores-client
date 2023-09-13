afbe179d57fb3f8c411c230ae0f35ad27e36ebb7
Ramit Suri <ramitsuri007@gmail.com>
2023-09-13 06:02:27 -0400

    Release 6.3 (48)

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -76,8 +76,8 @@ android {
         applicationId = "com.ramitsuri.choresclient.android"
         minSdk = (findProperty("android.minSdk") as String).toInt()
         targetSdk = (findProperty("android.targetSdk") as String).toInt()
-        versionCode = 47
-        versionName = "6.2"
+        versionCode = 48
+        versionName = "6.3"
 
         kapt {
             arguments {
